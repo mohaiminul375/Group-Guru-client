352eae940f0c6f6f4eb8640798f979cbb1e980c9
Mohaiminul375 <mohaiminul375@gmail.com>
2024-05-12 12:09:46 +0600

    data update successfully

--- src/components/Pages/UpdateAssignment/UpdateAssignment.jsx
+++ src/components/Pages/UpdateAssignment/UpdateAssignment.jsx
@@ -1,16 +1,42 @@
 import { useContext, useState } from "react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { AuthContext } from "../../../provider/AuthProvider";
 import DatePicker from "react-datepicker";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 const UpdateAssignment = () => {
-    const {data}=useLoaderData();
-    console.log(data);
-    const {assignment_title,difficulty_level,assignment_marks,due_date,photo_url,
-        assignment_description
-    }=data;
-    const [startDate, setStartDate] = useState(new Date());
-    // const {user}=useContext(AuthContext);
+    const navigate=useNavigate();
+  const { data } = useLoaderData();
+  // console.log(data);
+  const {
+    _id,
+    assignment_title,
+    difficulty_level,
+    assignment_marks,
+    due_date,
+    photo_url,
+    assignment_description,
+  } = data;
+  const [startDate, setStartDate] = useState(new Date(due_date));
+  // const {user}=useContext(AuthContext);
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (update) => {
+    update.due_date = startDate;
+    console.log("update data", update);
+    axios.patch(`http://localhost:5000/all-assignment/${_id}`, update)
+    .then(data=>{
+
+        console.log(data.data);
+        if(data.data.
+            modifiedCount){
+                toast.success('update successfully')
+                setTimeout(() => {
+                    navigate('/assignment')
+                }, 1000);
+            }
+    })
+  };
 
   return (
     <div className="mt-16 bg-[#024a5050] rounded-xl">
@@ -20,11 +46,11 @@ const UpdateAssignment = () => {
         </h2>
         <div>
           <form
-            // onSubmit={handleSubmit(onSubmit)}
+            onSubmit={handleSubmit(onSubmit)}
             className="w-full p-8 space-y-4"
           >
             {/* row1 */}
-            
+
             {/* row2 */}
             <div className="md:flex flex-row gap-5">
               <div className="form-control md:w-1/2">
@@ -32,12 +58,12 @@ const UpdateAssignment = () => {
                   <span className="text-base font-bold">Assignment Title</span>
                 </label>
                 <input
-                defaultValue={assignment_title}
+                  defaultValue={assignment_title}
                   type="text"
                   className="input input-bordered"
                   placeholder="enter assignment title"
                   required
-                //   {...register("assignment_title")}
+                  {...register("assignment_title")}
                 />
               </div>
               <div className="form-control md:w-1/2">
@@ -47,14 +73,12 @@ const UpdateAssignment = () => {
                   </span>
                 </label>
                 <select
-                defaultValue={difficulty_level}
+                  defaultValue={difficulty_level}
                   className="select select-bordered w-full"
-                //   {...register("difficulty_level")}
+                  {...register("difficulty_level")}
                   required
                 >
-                  <option
-                  
-                  value="">Please Select an option</option>
+                  <option value="">Please Select an option</option>
                   <option>Easy</option>
                   <option>Medium</option>
                   <option>Hard</option>
@@ -68,11 +92,11 @@ const UpdateAssignment = () => {
                   <span className="text-base font-bold">Assignment Marks</span>
                 </label>
                 <input
-                defaultValue={assignment_marks}
+                  defaultValue={assignment_marks}
                   type="number"
                   className="input input-bordered"
                   placeholder="you assignment marks"
-                //   {...register("assignment_marks")}
+                  {...register("assignment_marks")}
                   required
                 />
               </div>
@@ -82,7 +106,8 @@ const UpdateAssignment = () => {
                 </label>
                 <DatePicker
                   className="w-full h-[48px] input-bordered"
-                  selected={due_date}
+                  selected={startDate}
+                  //   defaultValue={due_date}
                   onChange={(date) => setStartDate(date)}
                   required
                 />
@@ -95,12 +120,12 @@ const UpdateAssignment = () => {
                   <span className="text-base font-bold">Photo URL</span>
                 </label>
                 <input
-                defaultValue={photo_url}
+                  defaultValue={photo_url}
                   type="text"
                   className="input input-bordered"
                   placeholder="input your photo url"
                   required
-                //   {...register("photo_url")}
+                  {...register("photo_url")}
                 />
               </div>
             </div>
@@ -111,14 +136,14 @@ const UpdateAssignment = () => {
                   <span className="text-base font-bold">Description</span>
                 </label>
                 <textarea
-                defaultValue={assignment_description}
+                  defaultValue={assignment_description}
                   name=""
                   id=""
                   rows={5}
                   placeholder="input assignment description "
                   className="rounded-md p-8"
                   required
-                //   {...register("assignment_description")}
+                  {...register("assignment_description")}
                 ></textarea>
               </div>
             </div>
